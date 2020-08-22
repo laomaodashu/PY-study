9872f97e84be8c797c93d5e760dd2acccc5b218c
laomaodashu <maomaocity@126.com>
2020-08-22 11:33:09 +0800

    2020.08.22 第一次向Github上传代码

---
+++ demo01.py
@@ -0,0 +1,46 @@
+'''
+print('你好') #字符串
+print('WTF')
+print(2333)
+print(2.3333)
+print(True)
+print(666)
+print("haha"+"xixi")
+
+print('哈哈')
+print('哈哈'*100)
+print('嘿嘿',2333,2.333)
+print(1+2)
+print(((1+2)*100-9.9)/2)
+print(2<3)
+
+name = '张三'
+print(name)
+'''
+
+
+# a = float(input('请输入：'))
+# b = float(input('请输入：'))
+# print('input获取的内容：',int(a+b))
+
+
+# # c = type(2333)
+# # print(c)
+
+# # print(type(True))
+# # print(type([]))
+
+# a = str('2333')
+# print(type(a))
+
+# a = '我操'
+# print(len(a))
+
+
+a = input('请输入内容一：')
+b = input('请输入内容二：')
+print('您输入的第一段内容是:',a)
+print('您输入的第一段内容的字符长度为：',len(a))
+print('您输入的第一段内容是:',b)
+print('您输入的第一段内容的字符长度为：',len(b))
+print('您输入的两段内容的字符长度之和为：',len(a+b))
